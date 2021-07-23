8178810bc7a0e20c15b80e8d885ba23f01e3ee93
nathanluu <nate1299@me.com>
2021-07-23 18:41:58 -0400

    Initial commit

---
+++ pkg/validation.go
@@ -0,0 +1,166 @@
+package pkg
+
+import (
+	"BrunoCoin/pkg/block"
+	"BrunoCoin/pkg/block/tx"
+)
+
+/*
+ *  Brown University, CS1951L, Summer 2021
+ *  Designed by: Colby Anderson, John Roy,
+ *	Parker Ljung
+ *
+ */
+
+
+// ChkBlk (CheckBlock) validates a block based on multiple
+// conditions.
+// To be valid:
+// The block must be syntactically (ChkBlkSyn), semantically
+// (ChkBlkSem), and configurally (ChkBlkConf) valid.
+// Each transaction on the block must be syntactically (ChkTxSyn),
+// semantically (ChkTxSem), and configurally (ChkTxConf) valid.
+// Each transaction on the block must reference UTXO on the same
+// chain (main or forked chain) and not be a double spend on that
+// chain.
+// Inputs:
+// b *block.Block the block to be checked for validity
+// Returns:
+// bool True if the block is valid. false
+// otherwise
+// TODO:
+// to be valid
+
+// Each transaction on the block must reference UTXO on the same
+// chain (main or forked chain) and not be a double spend on that
+// chain.
+// The block's size must be less than or equal to the largest
+// allowed block size.
+// The block hash must be less than the difficulty target.
+// The block's first transaction must be of type Coinbase.
+
+// Some helpful functions/methods/fields:
+// note: let t be a transaction object
+// note: let b be a block object
+// t.IsCoinbase()
+// b.SatisfiesPOW(...)
+// n.Conf.MxBlkSz
+// b.Sz()
+// n.Chain.ChkChainsUTXO(...)
+func (n *Node) ChkBlk(b *block.Block) bool {
+	if b == nil {
+		return false
+	}
+
+	if b.Sz() > n.Conf.MxBlkSz {
+		return false
+	}
+
+	if !b.SatisfiesPOW(n.Mnr.DifTrg()) {
+		return false
+	}
+
+	if !n.Chain.ChkChainsUTXO(b.Transactions, b.Hdr.PrvBlkHsh) {
+		return false
+	}
+
+	if len(b.Transactions) < 1 {
+		return false
+	}
+
+	for txIndex := range b.Transactions {
+		txn := b.Transactions[txIndex]
+
+		if txn == nil {
+			return false
+		}
+
+		if !n.ChkTx(txn) {
+			return false
+		}
+
+		if txn.IsCoinbase() && txIndex != 0 {
+			return false
+		}
+
+		if len(txn.Inputs) < 1 && !txn.IsCoinbase() {
+			return false
+		}
+
+	}
+	return true
+}
+
+
+// ChkTx (CheckTransaction) validates a transaction.
+// Inputs:
+// t *tx.Transaction the transaction to be checked for validity
+// Returns:
+// bool True if the transaction is syntactically valid. false
+// otherwise
+// TODO:
+// to be valid:
+
+// The transaction's inputs and outputs must not be empty.
+// The transaction's output amounts must be larger than 0.
+// The sum of the transaction's inputs must be larger
+// than the sum of the transaction's outputs.
+// The transaction must not double spend any UTXO.
+// The unlocking script on each of the transaction's
+// inputs must successfully unlock each of the corresponding
+// UTXO.
+// The transaction must not be larger than the
+// maximum allowed block size.
+
+// Some helpful functions/methods/fields:
+// note: let t be a transaction object
+// note: let b be a block object
+// note: let u be a transaction output object
+// n.Conf.MxBlkSz
+// t.Sz()
+// u.IsUnlckd(...)
+// n.Chain.GetUTXO(...)
+// n.Chain.IsInvalidInput(...)
+// t.SumInputs()
+// t.SumOutputs()
+func (n *Node) ChkTx(t *tx.Transaction) bool {
+	if t == nil {
+		return false
+	}
+
+	if t.SumOutputs() <= 0 {
+		return false
+	}
+
+	if t.SumInputs() < t.SumOutputs() {
+		return false
+	}
+
+	if t.Sz() > n.Conf.MxBlkSz {
+		return false
+	}
+
+	if len(t.Inputs) < 1 && !t.IsCoinbase() {
+		return false
+	}
+
+	if t.Inputs == nil {
+		return false
+	}
+
+	for txInputIndex := range t.Inputs {
+		txInput := t.Inputs[txInputIndex]
+		if txInput == nil {
+			return false
+		}
+		if n.Chain.IsInvalidInput(txInput) {
+			return false
+		}
+
+		if !n.Chain.GetUTXO(txInput).IsUnlckd(txInput.UnlockingScript) {
+			return false
+		}
+	}
+
+	return true
+}
